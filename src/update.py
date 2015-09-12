e66364defad845797951f3d4d3a22e07e07b1506
6H057WH1P3 <6H057WH1P3@users.noreply.github.com>
2015-09-12 13:03:51 +0200

    updater is ready for debugging

--- src/update.py
+++ src/update.py
@@ -6,75 +6,94 @@ import requests
 
 class GithubUpdater:
 
-    def __init__(self, project_name, actual_version):
+    def __init__(self, author, project_name, actual_version):
         self.actual_version = actual_version
-        self.project_url = "https://github.com/6H057WH1P3/"
+        self.project_url = "https://github.com/" + author + "/" + project_name + "/"
+        self.latest_version = ""
+        self.update_zip = ""
+        self.update_dir = "./update"
+        self.project_dir = "."
 
-    def check(project_name, actual_version):
-        print("[*] Checking for updates")
-        project_url = "https://github.com/6H057WH1P3/" + project_name + "/"
-        release_url = project_url + "releases"
+    def check(self):
+        release_url = self.project_url + "releases"
         release_request = requests.get(release_url)
-        search_text = '<span class="tag-name">'
-        text_length = len(search_text)
-        position = release_request.text.find(search_text)
-        if position == -1:
-            print("[-] There are no releases available for this project.")
-            return 0
-        latest_version = release_request.text[position + text_length : position + text_length + 5]
+        begin_text = '<span class="tag-name">'
+        begin = release_request.text.find(begin_text)
+
+        if begin == -1:
+            raise RuntimeWarning("There are no releases available for this project.")
+
+        begin += len(begin_text)
+        end = release_request.text.find('</span>', begin)
+        self.latest_version = release_request.text[begin : end]
         if actual_version >= latest_version:
-            print("[+] Script is already up to date")
             return 0
-        print("[*] A new update is available")
+        else:
+            return 1
 
-    print("[*] Starting download")
-    update_url = project_url + "archive/" + latest_version + ".zip"
-    update_zip = latest_version + ".zip"
-    with open(update_zip, "wb") as handle:
+    def download(self):
+        update_url = self.project_url + "archive/" + self.latest_version + ".zip"
+        self.update_zip = self.latest_version + ".zip"
+        with open(self.update_zip, "wb") as handle:
+            try:
+                download = requests.get(update_url, stream=True)
+            except:
+                raise RuntimeError("Download failed")
+
+            if not download.ok:
+                raise RuntimeError("Update could not be retrieved successfully.")
+
+            for block in download.iter_content(1024):
+                handle.write(block)
+
+    def extract(self):
         try:
-            download = requests.get(update_url, stream=True)
+            with zipfile.ZipFile(self.update_zip, "r") as handle:
+                handle.extractall(self.update_dir)
         except:
-            print("[-] Download failed")
-            return 1
-        if not download.ok:
-            print("[-] Update could not be retrieved successfully.")
-            return 1
-        for block in download.iter_content(1024):
-            handle.write(block)
-    print("[+] Download finished successfully")
+            raise RuntimeError("Extraction failed")
 
-    print("[*] Extracting archive")
-    update_dir = "./update"
-    project_dir = "."
-    try:
-        with zipfile.ZipFile(update_zip, "r") as update_zip:
-            update_zip.extractall(update_dir)
-    except:
-        print("[-] Extraction failed")
-        return 1
-    print("[+] Extraction finished successfully")
+    def apply(self):
+        try:
+            for src_dir, dirs, files in os.walk(self.update_dir):
+                dst_dir = src_dir.replace(self.update_dir, self.project_dir)
+                if not os.path.exists(dst_dir):
+                    os.mkdir(dst_dir)
+                for file_ in files:
+                    src_file = os.path.join(src_dir, file_)
+                    dst_file = os.path.join(dst_dir, file_)
+                    if os.path.exists(dst_file):
+                        os.remove(dst_file)
+                    shutil.move(src_file, dst_dir)
+        except:
+            raise RuntimeError("Critical update error")
 
-    print("[*] Updating content, do not cancel the script now")
-    try:
-        for src_dir, dirs, files in os.walk(update_dir):
-            dst_dir = src_dir.replace(update_dir, project_dir)
-            if not os.path.exists(dst_dir):
-                os.mkdir(dst_dir)
-            for file_ in files:
-                src_file = os.path.join(src_dir, file_)
-                dst_file = os.path.join(dst_dir, file_)
-                if os.path.exists(dst_file):
-                    os.remove(dst_file)
-                shutil.move(src_file, dst_dir)
-    except:
-        print("[!] Critical update error")
-        input("[*] Press Enter to continue")
-    try:
-        os.remove(update_zip)
-        shutil.rmtree(update_dir)
-    except:
-        print("[-] Cant delete update files")
+    def clear(self):
+        try:
+            os.remove(self.update_zip)
+            shutil.rmtree(self.update_dir)
+        except:
+            raise RuntimeError("Cant delete update files")
+        return 0
 
-    print("[+] Update finished successfully")
+    def update(self):
+        try:
+            print("[*] Checking for updates")
+            if not self.check():
+                print("[+] Script is already up to date")
+                return 0
+            print("[*] A new update is available")
 
-    return 0
+            print("[*] Starting download")
+            self.download()
+            print("[+] Download finished successfully")
+
+            print("[*] Extracting archive")
+            self.extract()
+            print("[+] Extraction finished successfully")
+
+            print("[*] Updating content, do not cancel the script now")
+            self.apply()
+            print("[+] Update finished successfully")
+        except Exception as e:
+            print("[!] Update Error: " + str(e))
