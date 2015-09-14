44d50198d554c89e278235119d94895e650ce991
6H057WH1P3 <6H057WH1P3@users.noreply.github.com>
2015-09-14 09:46:36 +0200

    Debugged a bit, now it's release time !

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ import updater
 
 AUTHOR = "6H057WH1P3"
 PROJECT = "Asit"
-VERSION = "v1.1.4"
+VERSION = "v1.1.5"
 
 def main():
     accounts_path = "./data/accounts.txt"
     # check for updates
-    updater = updater.GithubUpdater(AUTHOR, PROJECT, VERSION)
-    updater.update()
+    update_handler = updater.GithubUpdater(AUTHOR, PROJECT, VERSION)
+    update_handler.update()
     # just do the thing
     bot = asit.ManageAccounts(accounts_path)
     bot.Manage()
